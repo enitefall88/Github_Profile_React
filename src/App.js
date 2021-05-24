5564876ccfd62b4c91d3bf75da848424dc547537
Anton <enitefall81@gmail.com>
2021-05-23 19:34:22 -0700

    Set-up

--- src/App.js
+++ src/App.js
@@ -1,6 +1,18 @@
-import React from "react";
+import React, {useEffect} from "react";
 
 function App() {
+  useEffect(() => {
+    let interval
+    if(running) {
+     interval = setInterval(() => {
+      setTimeLeft(count => count - 0.1)
+    }, 500)
+      return () => {
+      clearInterval(interval)
+      }
+  }}, [running])
+})
+
   return <div>
 
     </div>
