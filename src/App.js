f7adddd4e3d5655ff14450b572551898720bfd96
Anton <enitefall81@gmail.com>
2021-06-08 21:29:55 -0700

    Add layout

--- src/App.js
+++ src/App.js
@@ -11,8 +11,18 @@ function App() {
       )
 
 
-  return <div>
-
+   return <div className="p-3">
+    <h1 className="h3">GitHub Profile</h1>
+    <img width="150px" src={user.avatar_url}/>
+    <h2 className="h4">
+      {user.name}
+    </h2>
+    <p>
+      {user.bio}
+    </p>
+    <pre className="p-3" style={{background: "gray", color: "white"}}><code>
+      {JSON.stringify(user, null, 2)}
+    </code></pre>
   </div>
 }
 
