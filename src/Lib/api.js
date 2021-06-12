19356f4b1c12e99566b4debcbedc120e5aa5133d
Anton <enitefall81@gmail.com>
2021-06-11 19:57:08 -0700

    All done

--- src/Lib/api.js
+++ src/Lib/api.js
@@ -1,29 +1,34 @@
-import fetch from "cross-fetch"
-import * as R from "ramda"
-export function makeApiClient(apiOrigin) {
-  async function fetchJSON(url, options = {}) {
-    if (!url.startsWith("http")) {
-      url = apiOrigin + url
-    }
-    options = R.mergeDeepRight(options, {
-      headers: {
-        "Accept": "application/json",
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(options.body),
-    })
-    let resp = await fetch(url, options)
-    if ((resp.headers.get("content-type") || "").includes("application/json")) {
-      try {
-        return resp.json()
-      } catch (err) {
-        throw new Error(`API: Invalid JSON`)
-      }
-    } else {
-      throw new Error(`API: Invalid mime-type`)
-    }
+import React, {useState, useEffect} from "react"
+import {makeApiClient} from "./lib/api"
+let apiClient = makeApiClient("https://api.github.com")
+// 5.6, 5.7, 5.8
+export default function App() {
+  let [user, setUser] = useState(null)
+  let [loading, setLoading] = useState(true)
+  useEffect(_ => {
+    apiClient.fetchJSON("/users/ivan-kleshnin")
+      .then(user => {
+        setUser(user)
+        setLoading(false)
+      })
+  }, [])
+  if (loading) {
+    return <Loading/>
   }
-  return {
-    fetchJSON,
-  }
+  return <div className="p-3">
+    <h1 className="h3">GitHub Profile</h1>
+    <img width="150px" src={user.avatar_url}/>
+    <h2 className="h4">
+      {user.name}
+    </h2>
+    <p>
+      {user.bio}
+    </p>
+    <pre className="p-3" style={{background: "gray", color: "white"}}><code>
+      {JSON.stringify(user, null, 2)}
+    </code></pre>
+  </div>
+}
+function Loading() {
+  return <span>Loading...</span>
 }
