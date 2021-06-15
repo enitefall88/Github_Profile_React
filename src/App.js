1e0f17add7553312a294fcc900ab85e935787479
Anton <enitefall81@gmail.com>
2021-06-15 13:45:35 -0700

    Fixes

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
-import React, {useEffect} from "react";
+import React, {useState, useEffect} from "react"
 
-
-
-function App() {
-  fetch("http://api.github.com/users/ivan-kleshnin")
-      .then(resp => {
-            resp.json()
-      .then(data => console.log(data))
-          }
-      )
-
-
-   return <div className="p-3">
+import {makeApiClient} from "./Lib/api"
+let apiClient = makeApiClient("https://api.github.com")
+// 5.6, 5.7, 5.8
+export default function App() {
+  let [user, setUser] = useState(null)
+  let [loading, setLoading] = useState(true)
+  useEffect(_ => {
+    apiClient.fetchJSON("/users/ivan-kleshnin")
+      .then(user => {
+        setUser(user)
+        setLoading(false)
+      })
+  }, [])
+  if (loading) {
+    return <Loading/>
+  }
+  return <div className="p-3">
     <h1 className="h3">GitHub Profile</h1>
     <img width="150px" src={user.avatar_url}/>
     <h2 className="h4">
@@ -25,9 +30,6 @@ function App() {
     </code></pre>
   </div>
 }
-
 function Loading() {
   return <span>Loading...</span>
 }
-
-export default App;
